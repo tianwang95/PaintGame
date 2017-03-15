c9d97e70d236bf5a58099005d1817e71e83bae93
Tian Wang <twang95@stanford.edu>
2017-03-14 20:43:41 -0700

    Added weapon switching

--- Assets/Scripts/Main/Player.cs
+++ Assets/Scripts/Main/Player.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public GameObject[] initWeapons;
+
 	[SerializeField]
-	private GameObject weaponBase;
+	private List<GameObject> weapons;
 	[SerializeField]
 	private float amountForward;
 	[SerializeField]
 	private float amountRight;
+	[SerializeField]
+	private float amountLeft;
 	private GameObject currWeapon;
 	private int weaponIdx = 0;
 
 	// Use this for initialization
 	void Start () {
-		gameObject.AddComponent<Inventory> ();
-		currWeapon = Instantiate (weaponBase, transform.position + transform.forward * amountForward + transform.right * amountRight, transform.rotation);
-		currWeapon.transform.parent = Camera.main.transform;
+		foreach (GameObject weapon in initWeapons) {
+			PickUpWeapon (weapon);
+		}
 	}
 	
 	// Update is called once per frame
@@ -35,16 +39,57 @@ public class Player : MonoBehaviour {
 				weapon.SecondaryFire ();
 			}
 		}
-		//TODO switch weapons
+		if (Input.GetButtonDown("SwitchWeapon") && Input.GetAxis("SwitchWeapon") > 0) {
+			CycleWeapon (1);
+		}
+		if (Input.GetButtonDown("SwitchWeapon") && Input.GetAxis ("SwitchWeapon") < 0) {
+			CycleWeapon (-1);
+		}
 	}
 
-	void PickUpWeapon(IWeapon weapon) {
-		Inventory inv = (Inventory) gameObject.GetComponent(typeof(Inventory));
-		int initSize = inv.size ();
-		inv.AddWeapon (weapon);
-		if (inv.size () > initSize) {
-			//it's a new weapon
-			currWeapon = weapon.CreateGameObject ();
+	void PickUpWeapon(GameObject weapon) {
+		if (weapon.GetComponent<IWeapon> () == null) {
+			Debug.LogError ("Tried to pick up GameObject without IWeapon as a weapon");
+			return;
+		}
+		GameObject existingWeapon = FindWeapon (weapon);
+		if (existingWeapon != null) {
+			existingWeapon.GetComponent<IWeapon> ().AddAmmo (weapon.GetComponent<IWeapon> ().GetAmmoCount ());
+		} else {
+			GameObject weaponClone = Instantiate (weapon,
+				transform.position + transform.forward * amountForward + transform.right * amountRight,
+				transform.rotation);
+			weaponClone.transform.parent = Camera.main.transform;
+			weaponClone.SetActive (false);
+			weapons.Add (weaponClone);
+			CycleWeapon (1);
+		}
+	}
+
+	void CycleWeapon(int index) {
+		if (weapons.Count > 0) {
+			weaponIdx = ((weaponIdx += index) % weapons.Count + weapons.Count) % weapons.Count;
+			GameObject nextWeapon = weapons [weaponIdx];
+			if (currWeapon != null) {
+				currWeapon.SetActive (false);
+			}
+			nextWeapon.SetActive (true);
+			currWeapon = nextWeapon;
+		}
+	}
+
+	GameObject FindWeapon(GameObject weapon) {
+		IWeapon weaponComponent = weapon.GetComponent<IWeapon> ();
+		if (weaponComponent == null) {
+			Debug.LogError ("Tried to check GameObject without IWeapon as a weapon");
+			return null;
+		} else {
+			foreach (GameObject w in weapons) {
+				if (w.GetComponent<IWeapon>().GetType () == weapon.GetComponent<IWeapon>().GetType ()) {
+					return w;
+				}
+			}
+			return null;
 		}
 	}
 }
